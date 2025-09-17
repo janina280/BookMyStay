a57e02407fb74288d1621ee099cf4ed694dba584
janina280 <coceijanina@yahoo.com>
2025-09-17 16:10:46 +0300

    write custom user details service

--- book-my-stay-server/src/main/java/com/bookmystay/BookMyStay/entity/User.java
+++ book-my-stay-server/src/main/java/com/bookmystay/BookMyStay/entity/User.java
@@ -63,4 +63,9 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
 }
