49cb796d80321195ccaaf3bfb184555c332bee7f
janina280 <coceijanina@yahoo.com>
2025-10-09 17:30:22 +0300

    user service

--- book-my-stay-server/src/main/java/com/bookmystay/BookMyStay/service/implementation/UserService.java
+++ book-my-stay-server/src/main/java/com/bookmystay/BookMyStay/service/implementation/UserService.java
@@ -2,28 +2,118 @@ package com.bookmystay.BookMyStay.service.implementation;
 
 import com.bookmystay.BookMyStay.dto.LoginRequest;
 import com.bookmystay.BookMyStay.dto.Response;
+import com.bookmystay.BookMyStay.dto.UserDTO;
 import com.bookmystay.BookMyStay.entity.User;
+import com.bookmystay.BookMyStay.exception.OurException;
+import com.bookmystay.BookMyStay.repository.UserRepository;
 import com.bookmystay.BookMyStay.service.interfac.IUserService;
+import com.bookmystay.BookMyStay.utils.JWTUtils;
+import com.bookmystay.BookMyStay.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService implements IUserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
-    public Response register(User loginRequest) {
-        return null;
+    public Response register(User user) {
+        Response response=new Response();
+        try {
+            if(user.getRole()==null || user.getRole().isBlank()){
+                user.setRole("USER");
+            }
+            if(userRepository.existsByEmail(user.getEmail())){
+                throw new OurException(user.getEmail()+"Already Exists");
+            }
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User saveUser=userRepository.save(user);
+            UserDTO userDTO= Utils.mapUserEntityToUserDTO(saveUser);
+            response.setStatusCode(200);
+            response.setUser(userDTO);
+        }
+        catch (OurException e){
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response login(LoginRequest loginRequest) {
-        return null;
+        Response response= new Response();
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getParssword()));
+            var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(()->new OurException("User not found! "));
+
+            var token= jwtUtils.generateToken(user);
+            response.setStatusCode(200);
+            response.setToken(token);
+            response.setRole(user.getRole());
+            response.setExpirationTime("7 Days");
+            response.setMessage("successful");
+
+        }
+        catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getAllUsers() {
-        return null;
+        Response response= new Response();
+        try {
+            List<User> userList= userRepository.findAll();
+            List<UserDTO> userDtoList= Utils.mapUserListEntityToUserDTO(userList);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUserList(userDtoList);
+        }
+        catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getUserBookingHistory(String userId) {
-        return null;
+        Response response= new Response();
+        try {
+            User user= userRepository.findById(Long.valueOf(userId)).orElseThrow(()-> new OurException("User not found!"));
+
+        }
+        catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
